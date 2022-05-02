d83c5af4ee8d1c483392c50c97da1046cd12fc23
Grant-Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-05-02 17:31:37 +0200

    Add functions and emoticon.
    Add function and emoticon
    altered from the textbook I am using
    to learn Node.js, replaced chars with the
    letters of my name.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function upcase(strings, ...values) {
-    
+
     return values.map(name => name[0].toUpperCase() + name.slice(1)
-    .join(' ') + strings[2]);
+        .join(' ') + strings[2]);
 }
 
 const person = {
@@ -11,3 +11,14 @@ const person = {
     position: ''
 
 };
+
+const { first, last } = person;
+
+const emoticon = [
+    ['G', '('],
+    ['r', 'a'],
+    ['nt', ',', ''],
+    ['`,']
+];
+
+console.log(upcase `${first} ${last} is the creator of this file!` + emoticon.flat().join(''));
